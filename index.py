a1fa8d1f37701eda742c0926ac289351c40dea35
Alyssa Cuson <90721438+acuson@users.noreply.github.com>
2022-07-20 10:36:53 -0400

    updated with comments

--- index.py
+++ index.py
@@ -5,11 +5,12 @@ from bs4 import BeautifulSoup
 http = urllib3.PoolManager()
 
 # Replace URL with the one that you are checking a GTM container for
-response = http.request('GET', "https://www.carvyktihcp.com/")
+response = http.request('GET', "https://www.edurant.com/")
 soup = BeautifulSoup(response.data,"html.parser")
 GTM = soup.find_all("noscript")
 
-# Replace with GTM container you wish to look for
+# Replace with GTM container you wish to look for or use this regex script to find any GTM container present
+# GTM-[A-Z0-9]{6,7}
 print(re.search("GTM-PD7QX3T", str(GTM))[0])
 
 # If it prints with the GTM container you wish to see, then the container is present on the site

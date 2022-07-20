cccd68e42858d38c8e0cd08adc4f5a25f1d71f78
Alyssa Cuson <90721438+acuson@users.noreply.github.com>
2022-07-20 09:52:20 -0400

    uploading web scaper

---
+++ index.py
@@ -0,0 +1,16 @@
+import urllib3
+import re
+from bs4 import BeautifulSoup
+
+http = urllib3.PoolManager()
+
+# Replace URL with the one that you are checking a GTM container for
+response = http.request('GET', "https://www.carvyktihcp.com/")
+soup = BeautifulSoup(response.data,"html.parser")
+GTM = soup.find_all("noscript")
+
+# Replace with GTM container you wish to look for
+print(re.search("GTM-PD7QX3T", str(GTM))[0])
+
+# If it prints with the GTM container you wish to see, then the container is present on the site
+# If it prints an error, then the GTM container is NOT present on the site
